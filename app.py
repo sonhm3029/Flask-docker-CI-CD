f3397e2ad419fa94c9bd98d62087af1037cb0632
sonhm2329 <hoangminhson29102001@gmail.com>
2023-07-09 13:29:57 +0700

    Thử push xem CI/CD jenkins hoạt động build

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return 'Hello, Docker with CI/CD!'
 
 
 @app.route("/widgets")
--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-docker run --rm -d --network mysqlnet --name rest-server -p 8000:5000 python-docker-dev
+docker run --name jenkins-docker --rm --detach --privileged --network jenkins --network-alias docker --env DOCKER_TLS_CERTDIR=/certs --volume jenkins-docker-certs:/certs/client --volume jenkins-data:/var/jenkins_home --publish 2376:2376 docker:dind --storage-driver overlay2
